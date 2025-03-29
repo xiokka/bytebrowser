ddb731e88276deeab554f38c60955fc698a227a9
Xiokka <xiokka@proton.me>
2025-03-29 02:30:17 -0400

    Update html.rs

--- src/html.rs
+++ src/html.rs
@@ -4,7 +4,7 @@ pub const microblog_html:&str = r#"
 <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>webhex</title>
+        <title>bytebrowser</title>
         <style>
 body {
         margin: 0;
